e2707297a57561ad009a90d48aa6d43a2059afb6
Akshata Srivastava <akshatasrivastava294@gmail.com>
2026-08-25 00:51:31 +0530

    Time: 0 ms (100%), Space: 13.3 MB (92.37%) - LeetHub

---
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int j =0;
+        for(int i=0 ;i<s.length()&&j<t.length();i++){
+            if(s[i]==t[j]){
+                j++;
+            }
+       
+        }
+         return t.length()- j;      
+    }
+};
