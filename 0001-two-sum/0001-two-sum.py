99ec68e032dfd522a917f990fb2e2f775a431ebf
Akshata Srivastava <akshatasrivastava294@gmail.com>
2026-09-05 08:45:24 +0530

    Time: 1 ms (61.63%), Space: 13.3 MB (17.03%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        mp ={}
+        for i in range(len(nums)):
+            need = target - nums[i]
+            if need in mp:
+                return(mp[need],i)
+            mp[nums[i]]=i
