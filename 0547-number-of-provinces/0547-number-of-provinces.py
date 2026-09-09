d7140276e8492dcd63167bea1bc87e6a3a0bb579
Akshata Srivastava <akshatasrivastava294@gmail.com>
2026-09-09 11:36:47 +0530

    Time: 7 ms (73.1%), Space: 13.6 MB (30.71%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -8,12 +8,13 @@ class Solution(object):
         visited = [False]*n
         province = 0
         def dfs(city):
-            visited[city] = True
+            visited[city]=True
             for neighbour in range(n):
-                if isConnected[city][neighbour]==1 and visited[neighbour] ==0:
+                if(isConnected[city][neighbour]==1 and visited[neighbour]==0):
                     dfs(neighbour)
         for city in range(n):
             if visited[city]!=1:
-                province+=1
+                province += 1
                 dfs(city)
         return province
+        
