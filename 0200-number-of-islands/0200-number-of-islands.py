502d0ca44751815c0f0aae5463819411ef45a155
Akshata Srivastava <akshatasrivastava294@gmail.com>
2026-09-10 12:00:05 +0530

    Time: 227 ms (53.93%), Space: 29.5 MB (22.68%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -7,29 +7,28 @@ class Solution(object):
         count = 0
         rows = len(grid)
         cols = len(grid[0])
-        visited=[[False]*cols for _ in range(rows)]
+        visited = [[False]*cols for _ in range(rows)]
         def dfs(row,col):
-            visited[row][col] = True
-            new_row ,new_col = row+1,col
-            if  new_row >= 0 and new_row < rows and new_col >= 0 and new_col < cols and grid[new_row][new_col]=='1' and not  visited[new_row][new_col]:
+            visited[row][col]=True
+            new_row,new_col = row+1,col
+            if new_row >=0 and new_row < rows and new_col >=0 and new_col < cols and grid[new_row][new_col]=='1'and not visited[new_row][new_col]:
                 dfs(new_row,new_col)
-            new_row ,new_col = row-1,col
-            if  new_row >= 0 and new_row < rows and new_col >= 0 and new_col < cols and grid[new_row][new_col]=='1' and not   visited[new_row][new_col]:
+            new_row,new_col =row-1,col
+            if new_row >=0 and new_row < rows and new_col >=0 and new_col < cols and grid[new_row][new_col]=='1'and not visited[new_row][new_col]:
                 dfs(new_row,new_col)
-            new_row ,new_col = row,col-1
-            if  new_row >= 0 and new_row < rows and new_col >= 0 and new_col < cols and grid[new_row][new_col]=='1' and not   visited[new_row][new_col]:
+            new_row,new_col = row,col-1
+            if new_row >=0 and new_row < rows and new_col >=0 and new_col < cols and grid[new_row][new_col]=='1'and not visited[new_row][new_col]:
                 dfs(new_row,new_col)
-            new_row ,new_col = row,col+1
-            if  new_row >= 0 and new_row < rows and new_col >= 0 and new_col < cols and grid[new_row][new_col]=='1' and not   visited[new_row][new_col]:
+            new_row,new_col = row,col+1
+            if new_row >=0 and new_row < rows and new_col >=0 and new_col < cols and grid[new_row][new_col]=='1'and not visited[new_row][new_col]:
                 dfs(new_row,new_col)
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c]== '1' and  not  visited[r][c]  :
+                if grid[r][c] == '1' and  not visited[r][c]:
                     count+=1
-                    dfs(r,c)     
-        return count     
+                    dfs(r,c)
+        return count
 
 
 
-
-                
+        
